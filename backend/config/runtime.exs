e540fcb727b9cf66bd7bf4a905b31115dd6fcc8a
tripathi-03 <mani03divy@gmail.com>
2025-12-31 21:21:13 +0530

    Update runtime config for production

--- backend/config/runtime.exs
+++ backend/config/runtime.exs
@@ -1,30 +1,33 @@
 import Config
 
+# Enable Phoenix server in releases
 if System.get_env("PHX_SERVER") do
   config :backend, BackendWeb.Endpoint, server: true
 end
 
 config :backend, BackendWeb.Endpoint,
-  http: [port: String.to_integer(System.get_env("PORT") || "4000")]
+  http: [
+    ip: {0, 0, 0, 0},
+    port: String.to_integer(System.get_env("PORT", "4000"))
+  ]
 
 if config_env() == :prod do
   database_url =
-    System.get_env("DATABASE_URL") ||
-      raise "DATABASE_URL is missing"
+    System.fetch_env!("DATABASE_URL")
+
+  secret_key_base =
+    System.fetch_env!("SECRET_KEY_BASE")
 
   config :backend, Backend.Repo,
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    parameters: [sslmode: "disable"]
-
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise "SECRET_KEY_BASE is missing"
-
-  host = System.get_env("PHX_HOST") || "example.com"
+    ssl: true,
+    ssl_opts: [
+      verify: :verify_none
+    ],
+    pool_size: String.to_integer(System.get_env("POOL_SIZE", "10"))
 
   config :backend, BackendWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [ip: {0, 0, 0, 0}, port: String.to_integer(System.get_env("PORT") || "4000")],
-    secret_key_base: secret_key_base
+    url: [host: System.get_env("PHX_HOST", "localhost"), port: 443],
+    secret_key_base: secret_key_base,
+    force_ssl: [rewrite_on: [:x_forwarded_proto]]
 end
