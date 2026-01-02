bbcb8a19db47baf5daf0e56c41c6ae9a6abafc22
tripathi-03 <mani03divy@gmail.com>
2026-01-03 02:11:20 +0530

    fixing CORS

--- backend/lib/backend_web/endpoint.ex
+++ backend/lib/backend_web/endpoint.ex
@@ -1,16 +1,6 @@
 defmodule BackendWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :backend
 
-  # Override error rendering to ensure CORS headers are always present
-  @doc false
-  defoverridable render_errors: 2
-
-  def render_errors(conn, opts) do
-    # Add CORS headers before rendering errors
-    conn = cors_headers(conn, [])
-    super(conn, opts)
-  end
-
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
