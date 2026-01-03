c6ef5a6f201154cd88ca6f8ab6370b7ba2f8f0e0
tripathi-03 <mani03divy@gmail.com>
2026-01-03 10:45:39 +0530

    Fixing Verel & Render env

--- backend/lib/backend_web/controllers/cors_controller.ex
+++ backend/lib/backend_web/controllers/cors_controller.ex
@@ -2,8 +2,8 @@ defmodule BackendWeb.CORSController do
   use BackendWeb, :controller
 
   def options(conn, _params) do
-    # CORSPlug will add the necessary headers
-    # We just need to return a 204 No Content response
+    # CORS headers are handled by the endpoint's handle_preflight plug
+    # This controller is kept for compatibility but may not be needed
     conn
     |> put_status(:no_content)
     |> send_resp(204, "")
--- backend/mix.exs
+++ backend/mix.exs
@@ -39,7 +39,6 @@ defmodule Backend.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:cors_plug, "~> 3.0"},
       {:phoenix, "~> 1.8.3"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
